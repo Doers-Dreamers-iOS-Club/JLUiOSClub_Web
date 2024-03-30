9a060c3ee12f1473398d1e5d42adb1e80347af95
AQiu <aqiu_2003@icloud.com>
2024-03-30 13:22:11 +0800

    fix bugs: image shadow in dark mode add new.md to jump to the newest activity

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -9,17 +9,17 @@ const sidebar: DefaultTheme.Sidebar = {
           {
             text: '即将推出',
             items: [
-              {text: '解锁iPad生产力', link: '/'},
-              {text: '移动应用创新赛宣讲', link: '/'}
+              {text: 'MAIC宣讲会暨Vision Pro体验会', link: '240407_maic_and_vision_pro/'},
+              {text: '百团纳新', link: '/'}
             ]
           },
           {
             text: '往期活动',
             items: [
+              {text: '解锁iPad生产力', link: '/'},
               {text: '2023冬季WWDC-Swift特训营', link: '/'},
               {text: '购买Mac/iPad迎接高校生活——选购建议&经验分享', link: '/'},
               {text: 'macOS初体验', link: '/'},
-              {text: '百团纳新', link: '/'}
             ]
           }
         ]
---
+++ docs/new.md
@@ -0,0 +1,8 @@
+---
+title: 跳转中……
+---
+
+<script setup>
+// Redirect to recruitment page
+window.location.href = '/activities/240407_maic_and_vision_pro/'
+</script>
