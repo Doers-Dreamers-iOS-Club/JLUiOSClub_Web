af24853da9623de3c8cadc40c812fe57209cac13
AQiu <aqiu_2003@icloud.com>
2024-04-12 22:09:02 +0800

    Revert "add SF fonts (may contain bugs)"
    This reverts commit c13b92f66fa2dc5b8c228ce687d116740801e6e0.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,9 +3,8 @@ import {h, onMounted, watch, nextTick} from 'vue'
 import {useRoute} from "vitepress";
 import type {Theme} from 'vitepress'
 import mediumZoom from "medium-zoom";
-import DefaultTheme from 'vitepress/theme-without-fonts'
+import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import './fontface.css'
 import './custom.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
